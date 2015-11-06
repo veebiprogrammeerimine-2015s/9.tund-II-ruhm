e49a1d7eeca4e34e8b044747e0bcce0126e11b29
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-11-06 10:20:29 +0200

    Added lesson 5 files

---
+++ functions.php
@@ -0,0 +1,98 @@
+<?php
+	//kõik AB'iga seonduv
+	
+	// ühenduse loomiseks kasuta
+	require_once("../configglobal.php");
+	$database = "if15_romil_2";
+	
+	// paneme sessiooni käima, saame kasutada $_SESSION muutujaid
+	session_start();
+
+	
+	// lisame kasutaja ab'i
+	function createUser($create_email, $password_hash){
+		// globals on muutuja kõigist php failidest mis on ühendatud
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("INSERT INTO user_sample (email, password) VALUES (?, ?)");
+		$stmt->bind_param("ss", $create_email, $password_hash);
+		$stmt->execute();
+		$stmt->close();
+		
+		$mysqli->close();		
+	}
+	
+	//logime sisse
+	function loginUser($email, $password_hash){
+		
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("SELECT id, email FROM user_sample WHERE email=? AND password=?");
+		$stmt->bind_param("ss", $email, $password_hash);
+		$stmt->bind_result($id_from_db, $email_from_db);
+		$stmt->execute();
+		if($stmt->fetch()){
+			echo "kasutaja id=".$id_from_db;
+			
+			$_SESSION["id_from_db"] = $id_from_db;
+			$_SESSION["user_email"] = $email_from_db;
+			
+			//suunan kasutaja data.php lehele
+			header("Location: data.php");
+			
+			
+		}else{
+			echo "Wrong password or email!";
+		}
+		$stmt->close();
+		
+		$mysqli->close();
+	}
+	
+	
+	function createCarPlate($car_plate, $color){
+		// globals on muutuja kõigist php failidest mis on ühendatud
+		$mysqli = new mysqli($GLOBALS["servername"], $GLOBALS["server_username"], $GLOBALS["server_password"], $GLOBALS["database"]);
+		
+		$stmt = $mysqli->prepare("INSERT INTO car_plates (user_id, number_plate, color) VALUES (?, ?, ?)");
+		$stmt->bind_param("iss", $_SESSION["id_from_db"], $car_plate, $color);
+		
+		$message = "";
+		
+		if($stmt->execute()){
+			// see on tõene siis kui sisestus ab'i õnnestus
+			$message = "Edukalt sisestatud andmebaasi";
+			
+		}else{
+			// execute on false, miski läks katki
+			echo $stmt->error;
+		}
+		
+		$stmt->close();
+		$mysqli->close();
+
+		return $message;
+		
+	}
+	
+	
+	/*
+	// return sample
+	
+	function welcome($name){
+		$string = "Tere ".$name;
+		return $string;
+		
+		// mis on pärast returni seda ei käivitata
+		echo "hellooooo";
+		
+	}
+	
+	$str = welcome("Romil");
+	
+	echo $str;
+	
+	*/
+	
+	
+?>
